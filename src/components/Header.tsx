ad97555407d4de34882cba2630830b459085d296
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2024-01-09 11:07:08 +0100

    completed the navigation bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="bg-[#F6F4F4]">
       <div className="max-w-[1440] mx-auto flex gap-5 items-center px-5 lg:px-10">
         <div
-          className={`pt-3 pb-1 border-b-8  cursor-pointer ${
+          className={`pt-4 pb-2 border-b-8  cursor-pointer ${
             pageType === "business" ? "border-secondary text-secondary" : "border-transparent"
           }`}
           onClick={() => setPageType("business")}
@@ -14,7 +14,7 @@ const Header = () => {
           For Work & Business
         </div>
         <div
-          className={`pt-3 pb-1 border-b-8 cursor-pointer ${
+          className={`pt-4 pb-2 border-b-8 cursor-pointer ${
             pageType === "personal" ? "border-secondary text-secondary" : "border-transparent"
           }`}
           onClick={() => setPageType("personal")}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
-import '@mantine/notifications/styles.css';
-import "./index.css"
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
